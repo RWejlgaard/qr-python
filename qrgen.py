4740562aef050ac9433274a5a2269cf3c7bd1960
rwejlgaard <rasmus.wejlgaard@gmail.com>
2014-06-18 07:29:31 +0200

    First commit

---
+++ qrgen.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import time
+#navn = raw_input('Pakkens modtager: ')
+#dest = raw_input('Pakkens destination: ')
+#idag = datetime.time(hour,minute,second)
+
+
+code = (time.strftime("%Y%d%m%H%M%S"))
+print code
+
