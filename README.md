6d5101b94d3056f3c189900b0e777bec1f9ce879
rwejlgaard <rasmus.wejlgaard@gmail.com>
2014-06-27 12:06:32 +0100

    Small changes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-qr-python
+QR-Python
 =========
-QR-Projekt for TEC pakker
+QR-Projekt for TEC Cargo
 
-CONFIGURATION
+Configuration
 =============
 For at indstille hvor den givende maskine befinder sig skal qr.conf ændres.
 locations IDs er som følgende:
@@ -13,6 +13,12 @@ Fredriksberg = 3
 Lyngby = 4
 Gladsaxe = 5
 
-RUN
+Run
 ===
 For at køre skal du befinde dig i projekt-mappen, og køre kommandoen "python launch.py"
+
+Requirements
+============
+python
+python-gtk
+
--- launch.py
+++ launch.py
@@ -4,13 +4,12 @@ pygtk.require('2.0')
 import gtk
 import time
 import subprocess
-import os
 
 class QRPrint:
 
-    def callback(self, widget, data):
+    def callback(self, widget, data, local):
         tid = time.strftime("%y%m%d%H%S")
-        code = data + tid
+        code = data + local + tid
         print code
         com = 'sh execqr {0}'.format(code)
         subprocess.call(com.split() ,shell=False)
@@ -20,37 +19,51 @@ class QRPrint:
         return False
 
     def __init__(self):
+        if scan("1"):
+            location = "1"
+        elif scan("2"):
+            location = "2"
+        elif scan("3"):
+            location = "3"
+        elif scan("4"):
+            location = "4"
+        elif scan("5"):
+            location = "5"
+
         #Saetter vaerdier og opsaetter vindue
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_title("Print QR Code")
+        self.window.fullscreen()
         self.window.connect("delete_event", self.delete_event)
         self.window.set_border_width(100)
         self.box1 = gtk.HBox(False, 0)
         self.window.add(self.box1)
-        self.window.fullscreen()
-
+        
         #Tilfojer knapper og binder dem til callback()
         self.Hvidovre = gtk.Button("Hvidovre")
-        self.Hvidovre.connect("clicked", self.callback, "1")
+        self.Hvidovre.connect("clicked", self.callback, "1", location)
         self.box1.pack_start(self.Hvidovre, True, True, 0)
 
         self.Ballerup = gtk.Button("Ballerup")
-        self.Ballerup.connect("clicked", self.callback, "2")
+        self.Ballerup.connect("clicked", self.callback, "2", location)
         self.box1.pack_start(self.Ballerup, True, True, 0)
 
         self.Fredriksberg = gtk.Button("Fredriksberg")
-        self.Fredriksberg.connect("clicked", self.callback, "3")
+        self.Fredriksberg.connect("clicked", self.callback, "3", location)
         self.box1.pack_start(self.Fredriksberg, True, True, 0)
 
         self.Lyngby = gtk.Button("Lyngby")
-        self.Lyngby.connect("clicked", self.callback, "4")
+        self.Lyngby.connect("clicked", self.callback, "4", location)
         self.box1.pack_start(self.Lyngby, True, True, 0)
         
         self.Gladsaxe = gtk.Button("Gladsaxe")
-        self.Gladsaxe.connect("clicked", self.callback, "5")
+        self.Gladsaxe.connect("clicked", self.callback, "5", location)
         self.box1.pack_start(self.Gladsaxe, True, True, 0)
 
-        #Tjekker .conf og viser knapper
+        self.box1.show()
+        self.window.show()
+
+        #Tjekker .conf, indstiller location og viser knapper
         if scan("1") == True:
             self.Ballerup.show()
             self.Fredriksberg.show()
@@ -68,26 +81,21 @@ class QRPrint:
             self.Ballerup.show()
             self.Lyngby.show()
             self.Gladsaxe.show()
- 
+
         elif scan("4") == True:
             self.Hvidovre.show()
             self.Fredriksberg.show()
             self.Ballerup.show()
             self.Gladsaxe.show()
- 
+
         elif scan("5") == True:
             self.Hvidovre.show()
             self.Fredriksberg.show()
             self.Lyngby.show()
             self.Ballerup.show()
-        
+
         else:
             print "Forkert .conf"
-
-
- 
-        self.box1.show()
-        self.window.show()
 
 def scan(stringin):
     conf = open('qr.conf').read()
